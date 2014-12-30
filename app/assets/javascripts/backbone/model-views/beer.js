0ae108509a8015410d2aadb28c27482252ac487b
Evan Turner <evturn@gmail.com>
2014-12-29 19:25:37 -0500

    BeerView written

--- app/assets/javascripts/backbone/model-views/beer.js
+++ app/assets/javascripts/backbone/model-views/beer.js
@@ -1,3 +1,11 @@
 var BeerView = Backbone.View.extend({
-	
+	initialize: function() {
+		this.render();
+	},
+	template: _.template($('#beer-search-template').html()),
+	render: function() {
+		this.$el.html(this.template(beer.toJSON()));
+		$('#search').append(this.$el);
+		return this;
+	}
 });
