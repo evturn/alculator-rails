f85fb095c2b49c62885c8b36d2d19059f427a2b0
Evan Turner <evturn@gmail.com>
2014-12-31 14:27:47 -0500

    routes updated in rails with create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root          to: 'application#index'
-  get '/beers'   => 'application#beer'
+  get  '/beers'  => 'application#beer'
+  post '/rounds' => 'application#create'
 end
