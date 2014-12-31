d046dc416475db289486444bd4ee23a9fb3be5e7
Evan Turner <evturn@gmail.com>
2014-12-31 14:23:19 -0500

    model created in rails with a calculate function

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,23 @@
 class Round < ActionController::Base
 
+  def calculate()
+    sex    = self.sex
+    lbs    = self.lbs
+    hours  = self.hours
+    drinks = self.drinks
+    abv    = self.abv
+
+    rate = sex == "male" ? 0.73 : 0.66
+
+    bev_oz = drinks * 12
+    alc_oz = bev_oz * (abv * 0.01)
+
+    step_1 = (alc_oz * 5.14)
+    step_2 = (lbs * rate)
+    step_3 = (step_1 / step_2)
+    step_4 = (0.015 * hours)
+
+    self.bac   = '%.2f' % (step_3 - step_4)
+
+  end
 end
